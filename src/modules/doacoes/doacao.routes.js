8007b8827c0a3b6bb938df626073555c08bedde6
yagozt <yagozt.henrique@gmail.com>
2018-10-25 08:30:15 -0300

    retirando o não finalizado

--- src/modules/doacoes/doacao.routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router,
-doacaoController = require('./doacao.controller');
-
-const routes = new Router();
-
-routes.post('/doacao', doacaoController.createDoacao);
-routes.get('/doacao', doacaoController.getAll);
-
-module.exports = routes;
